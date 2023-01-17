03c73c0ec7ca8c25ca78c1491ff39ed66a77861e
shubanms <msshuban4@gmail.com>
2023-01-17 13:34:37 +0530

    Major changes

--- Dynamic Programming/Fibonacci numbers/FibonacciNumbers.py
+++
@@ -1,9 +0,0 @@
-class fibo:
-    def fibonacci(self , n):
-        if n <= 1:
-            return n
-        return fibo().fibonacci(n-1) + fibo().fibonacci(n-2)
-
-
-if __name__ == "__main__":
-	print(fibo().fibonacci(int(input("Enter the number: "))))
